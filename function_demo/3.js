8ace8d313885b67f9ed87df35d122d5b51dda2ac
lijianglong <118046398+lijianglong0506@users.noreply.github.com>
2022-11-23 17:26:03 +0800

    js之 this   函数 作用域

---
+++ function_demo/3.js
@@ -0,0 +1,18 @@
+// 'use strict'
+//变量的作用域
+var name = '龙'  //  全局  window
+// this 指向  调用方式
+// js 变量属于  作用域
+function func(){
+   /* var */ y = 2;  /*去掉var就变成了全局变量   但这种写法不太好*/
+    var name = '楠'  // func  内部 局部变量
+    {
+        //块级作用域
+        let x = 1;
+        var z = 3;  // es5 不支持块级作用域
+        let name = '海' 
+    }
+    console.log(y,z);
+}
+func();
+console.log(y);
