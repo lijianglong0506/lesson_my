7344bfd1419f996b880fad35f5068903161c3895
jianglong <2314515018@qq.com>
2023-02-21 16:48:40 +0800

    vuex购物车

---
+++ vue3/vuex/shopping-cart/src/components/ShoppingCart.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        ShoppingCart
+    </div>
+</template>
+
+<script setup>
+import { useStore } from 'vuex'
+import { computed } from 'vue'
+
+const store = useStore()
+const items = computed(() => store.state.cart.items)
+console.log(store.state.cart.items);
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue3/vuex/shopping-cart/src/store/modules/cart.js
@@ -0,0 +1,15 @@
+const state = {
+    items: [],
+    checkoutStatus: null
+}
+
+const mutations = {
+    pushProductsToCart() {
+
+    }
+}
+
+export default {
+    state,
+    mutations
+}
