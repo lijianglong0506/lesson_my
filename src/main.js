b4d1d75a3cdd9a5d5804e65f0d67140d10bfec72
lijianglong <118046398+lijianglong0506@users.noreply.github.com>
2023-03-11 21:39:51 +0800

    vue2.0项目

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import { createApp } from 'vue'
-import  router from './router'
+// 2.0 文档
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
-import 'lib-flexible/flexible' 
-import './assets/base.css'
-import vant from 'vant'
-const app = createApp(App)
+import router from './router'
+Vue.use(VueRouter) // 给vue 添加router 模块
+import './assets/stylus/index.styl'
 
-app  
-    .use(vant)
-    .use(router)
-    .mount('#app')
+new Vue({
+    el:'#app',
+    router,
+    render: h => h(App)
+})
--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,20 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import VueRouter from 'vue-router'
+import goods from '../views/goods/goods.vue'
 
-const routes = [ 
-        {
-            path:'/',
-            redirect : () => import('@/views/Home/index.vue')
-        },
-        {  
-            // 路由的懒加载
-            path:'/Home',
-            component: () => import('@/views/Home/index.vue')
-        },
-        {
-            path:'/Cell',
-            component: () => import('@/views/Cell/index.vue')
-        },
-        {
-            path:'/Shopping',
-            component: () => import('@/views/Shopping/index.vue')
-        },
-        {
-            path:'/Message',
-            component: () => import('@/views/Message/index.vue')
-        },
-        {
-            path:'/Mine',
-            component: () => import('@/views/Mine/index.vue')
-        },
-    ];
-    const router = createRouter({
-        history: createWebHashHistory(),
-        routes
+const routes = [{
+    path: '/',
+    // redirect:goods
+    component:goods
+},
+{
+    path: '/goods',
+    component: goods
+}]
+
+const router = new VueRouter({
+    mode: 'history',
+    linkActiveClass: 'active',
+    routes,
 })
-export default router
 
+export default router
